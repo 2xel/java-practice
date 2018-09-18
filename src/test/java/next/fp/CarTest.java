445bfdb593b46b8838c304e9be59bb50084e925d
jaemin <jaeminstar@gmail.com>
2018-09-18 16:13:04 +0900

    CarTest 익명 클래스를 람다를 통해 구현

--- src/test/java/next/fp/CarTest.java
+++ src/test/java/next/fp/CarTest.java
@@ -5,27 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
 public class CarTest {
-    @Test
-    public void 이동() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return true;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 1));
-    }
+	@Test
+	public void 이동() {
+		Car car = new Car("pobi", 0);
+		Car actual = car.move(() -> {
+			return true;
+		});
+		assertThat(actual).isEqualTo(new Car("pobi", 1));
+	}
 
-    @Test
-    public void 정지() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return false;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 0));
-    }
+	@Test
+	public void 정지() {
+		Car car = new Car("pobi", 0);
+		Car actual = car.move(() -> {
+			return false;
+		});
+		assertThat(actual).isEqualTo(new Car("pobi", 0));
+	}
 }
