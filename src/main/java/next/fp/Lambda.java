b986c32ee8e285284b50745647c08c316e53e7b8
jaemin <jaeminstar@gmail.com>
2018-09-18 16:41:29 +0900

    람다를 적용하지 않은 익명 클래스를 이용한 구현

--- src/main/java/next/fp/Lambda.java
+++ src/main/java/next/fp/Lambda.java
@@ -5,52 +5,61 @@ import java.util.List;
 public class Lambda {
 	public static void printAllOld(List<Integer> numbers) {
 		System.out.println("printAllOld");
-		
+
 		for (int number : numbers) {
-		    System.out.println(number);
+			System.out.println(number);
 		}
 	}
 
 	public static void printAllLambda(List<Integer> numbers) {
 		System.out.println("printAllLambda");
-		
-		numbers.forEach(System.out::println);		
+
+		numbers.forEach(System.out::println);
 	}
-	
+
 	public static void runThread() {
-		new Thread(new Runnable() {
-		    @Override
-		    public void run() {
-		        System.out.println("Hello from thread");
-		    }
+		new Thread(() -> {
+			System.out.println("Hello from thread");
 		}).start();
 	}
 
-	public static int sumAll(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        total += number;
-	    }
-	    return total;
+	public static int sum(List<Integer> numbers, Conditional c) {
+		int total = 0;
+		for (Integer number : numbers) {
+			if (c.test(number)) {
+				total += number;
+			}
+		}
+		return total;
 	}
-	
+
+	public static int sumAll(List<Integer> numbers) {
+		return sum(numbers, new Conditional() {
+
+			@Override
+			public boolean test(Integer number) {
+				return true;
+			}
+		});
+	}
+
 	public static int sumAllEven(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        if (number % 2 == 0) {
-	            total += number;
-	        }
-	    }
-	    return total;
+		return sum(numbers, new Conditional() {
+
+			@Override
+			public boolean test(Integer number) {
+				return (number % 2 == 0) ? true : false;
+			}
+		});
 	}
 
 	public static int sumAllOverThree(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	    	if (number > 3) {
-	    		total += number;
-	    	}
-	    }
-	    return total;
+		return sum(numbers, new Conditional() {
+			
+			@Override
+			public boolean test(Integer number) {
+				return (number > 3) ? true : false;
+			}
+		});
 	}
 }
